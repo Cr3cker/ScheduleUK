ecbffeac6fdac0776fc7ea76aeceaf71150df6bf
cr3cker <vladstar1606@mail.ru>
2023-01-18 17:50:06 +0100

    Refactor && docs adding

--- candle.py
+++ candle.py
@@ -6,35 +6,49 @@ from time import sleep
 
 
 def get_audience_page(name, driver, url):
+    '''Returns the url of the audience page'''
+
     driver.get(url)
     driver.find_element(By.CSS_SELECTOR, 'a[href="#panel_cast_miestnosti"]').click()
     input_tag = driver.find_element(By.XPATH, '//*[@id="showRooms"]')
     input_tag.send_keys(name)
+
     sleep(3)
     room = driver.find_element(By.XPATH, '//*[@id="list_rooms_box"]/ul/li/a')
     room.click()
     current_url = driver.current_url + '.csv'
+
     return current_url
 
 def get_teacher_page(name, driver, url):
+    '''Returns the url of the teacher page'''
+
     driver.get(url)
     driver.find_element(By.CSS_SELECTOR, 'a[href="#panel_cast_ucitelia"]').click()
     input_tag = driver.find_element(By.XPATH, '//*[@id="showTeachers"]')
     input_tag.send_keys(name)
+
     sleep(3)
     teacher = driver.find_element(By.XPATH, '//*[@id="list_teachers_box"]/ul/li/a')
     teacher.click()
     current_url = driver.current_url + '.csv'
+
     return current_url
 
 def get_lessons(url):
+    '''Returns the lessons of the teacher or audience stored in a html file'''
+
     r = requests.get(url)
     decoded_content = r.content.decode('utf-8')
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
     my_list = list(cr)
     df = pd.DataFrame(my_list[1:], columns=my_list[0]).set_index(['Deň'])
+
+    # Drop a column with NaN values
     df = df.drop('Poznámka', axis=1)
     html = df.to_html()
+
+    # Save the html file
     html_file = open('schedule.html', 'w')
     html_file.write(html)
     html_file.close()
@@ -42,8 +56,11 @@ def get_lessons(url):
 
 
 def get_data(type, argument, driver, url, r):
+    '''Returns the data from the cache or from the website'''
+
     key = f"{type}:{argument}"
     data = r.get(key)
+
     if data:
         print('Data from cache')
         return data.decode()
@@ -59,6 +76,7 @@ def get_data(type, argument, driver, url, r):
             url = get_audience_page(argument, driver, url)
             data = get_lessons(url)
             data = data.read().decode('utf-8')
+
         else:
             raise ValueError("Invalid type")
 
--- db.py
+++ db.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 
 async def create_table():
+    '''Create a table to store requests if it doesn't exist'''
+
     async with aiosqlite.connect('candle.db') as db:
         await db.execute(
             'CREATE TABLE IF NOT EXISTS requests '
@@ -10,10 +12,12 @@ async def create_table():
         await db.commit()
 
 async def save_to_db(url, user):
+    '''Save request to database'''
+
     async with aiosqlite.connect('candle.db') as db:
         await db.execute(
             'INSERT INTO requests VALUES (?, ?, ?)',
             (datetime.now(), url, user))
         await db.commit()
 
-        
+
